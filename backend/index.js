937588ff3ab8d62f62082473b60517bba4c83276
Shubhamnitjsr093 <99527655+Shubhamnitjsr093@users.noreply.github.com>
2024-05-10 22:52:22 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+
+app.use(express.json());
+// body{
+    //title: string
+    //description: string
+//}
+app.post("/todo",(req,res)=>{
+
+})
+app.post("/todos",(req,res)=>{
+
+})
+app.post("/comleted",(req,res)=>{
+
+})
